bbda9649e0e37fdebd40ade04a9b112eff4cf880
Ashley Sheng <51773182+yelhsams@users.noreply.github.com>
2023-04-16 19:21:40 -0400

    Object files for HollywoodNode, Movie, Actor

---
+++ Movie.java
@@ -0,0 +1,13 @@
+
+/**
+ * Represents a movie (name only).
+ *
+ * @author (Ashley, Linda, Agnes)
+ * @version (04/16/2023)
+ */
+public class Movie extends HollywoodNode
+{
+    public Movie(String n){
+        super(n);
+    }
+}
