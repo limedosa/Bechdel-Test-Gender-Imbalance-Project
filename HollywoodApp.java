04e47adbca37718af5d03fe48535966a846c6aa8
Ashley Sheng <51773182+yelhsams@users.noreply.github.com>
2023-04-16 19:28:52 -0400

    Update HollywoodApp.java

--- HollywoodApp.java
+++ HollywoodApp.java
@@ -9,6 +9,33 @@ import java.io.File;
 import java.io.IOException;
 
 public class HollyWoodApp {
+    
+    private Vector<LinkedList<T>> arcs;   // adjacency matrices of arcs
+    private Vector<T> vertices;   // values of vertices
+
+    public void addVertex (T vertex){
+        // add it to the vertices vector
+        vertices.add(vertex);
+
+        //indicate that the new vertex has no arcs to other vertices yet
+        arcs.add(new LinkedList<T>());
+    }
+
+    public boolean isArc (T vertex1, T vertex2){
+        try {
+            int index = vertices.indexOf(vertex1);
+            LinkedList<T> l = arcs.get(index);
+            return (l.indexOf(vertex2) != -1);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(vertex1 + " vertex does not belong in the graph");
+            return false;
+        }
+    }
+
+    public boolean isEdge(T vertex1, T vertex2) {
+        return (isArc(vertex1, vertex2) && isArc(vertex2, vertex1));
+    }
+    
     private Graph graph;
     public HollyWoodApp(){
         graph = new Graph();
